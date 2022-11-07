3ccf0d7eefbe85318a3724415edcfd6d642fa890
overol1221 <fiqiachmad12@gmail.com>
2022-11-07 18:03:14 +0700

    Add class program list

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -16,24 +16,116 @@ namespace singly_linked_list
 
     class list
     {
-        node start;
+        node START;
         public list()
         {
-            start = null;
+            START = null;
         }
         public void addnote() // add a note in the list
         {
             int rollNo;
             string nm;
-            Console.Write();
+            Console.Write("\nEnter the roll number of the student: ");
             rollNo = Convert.ToInt32(Console.ReadLine());
             Console.Write("\nEnter the roll name of the student: ");
             nm = Console.ReadLine();
             node newnode = new node();
             newnode.rollnumber = rollNo;
             newnode.name = nm;
+            if ((START != null) && (rollNo <= START.rollNumber)) ;
+            {
+                if ((START != null) && (rollNo == START.rollNumber)) ;
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                newnode.next = START;
+                START = newnode;
+                return;
+            }
+            node previous, current;
+            previous = START;
+            current = START;
+
+            while ((current != null) && (rollNo >= current.rollnumber))
+            {
+                if (rollNo == current.rollnumber)
+                {
+                    Console.WriteLine();
+                    return;
+                }
+                previous.next = current;
+                previous.next = newnode;
+            }
+            newnode.next = current;
+            previous.next = newnode;
+        }
+
+        public bool delnode(int rollNo)
+        {
+            node previous, current;
+            previous = current = null;
+            if (search(rollNo, ref previous, ref current) == false)
+                return false;
+            if (current == START)
+                START = START.next;
+            return true;
+        }
+        public bool search(int rollNo, ref node previous, ref node current)
+        {
+            previous = START;
+            current = START;
+            while ((current != null) && (rollNo != current.rollnumber))
+            {
+                previous = current;
+                current = current.next;
+            }
+            if (current == null)
+                return false;
+            else
+                return true;
+        }
+        public void travarse()
+        {
+            if (listEmpty())
+                Console.WriteLine();
+            else
+            {
+                Console.WriteLine();
+                node currentNode;
+                for (currentNode = START; currentNode != null ;
+                currentNode = currentNode.next)
+                    Console.Write(currentNode.rollnumber + "" + currentNode.rollnumber + " " + currentNode.name + "\n");
+                Console.WriteLine();
+            }
+        }
+        public bool listEmpty()
+        {
+            if (START == null)
+                return true;
+            else
+                return false;
         }
     }
+    class program
+    { 
+        static void Main(string[] args)
+        {
+            list obj = new list();
+            while(true)
+            { 
+                try
+                {
+                    Console.WriteLine("\nMenu");
+                    Console.WriteLine("1. add record to the list");
+                    Console.WriteLine("2. delete a record to the list");
+                    Console.WriteLine("3. view all the record from the list");
+                    Console.WriteLine("4. search for a record in the list");
+                    Console.WriteLine("5. exit");
+                    Console.Write("\nEnter your choice (1-5) :");
+        }
+       
+   
     internal class Program
     {
         static void Main(string[] args)
