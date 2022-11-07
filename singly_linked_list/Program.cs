9e06139f03d363013dea5db88830f46753564bc7
overol1221 <fiqiachmad12@gmail.com>
2022-11-07 16:57:52 +0700

    add class add note

--- singly_linked_list/Program.cs
+++ singly_linked_list/Program.cs
@@ -13,6 +13,27 @@ namespace singly_linked_list
         public string name;
         public node next;
     }
+
+    class list
+    {
+        node start;
+        public list()
+        {
+            start = null;
+        }
+        public void addnote() // add a note in the list
+        {
+            int rollNo;
+            string nm;
+            Console.Write();
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollnumber = rollNo;
+            newnode.name = nm;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
